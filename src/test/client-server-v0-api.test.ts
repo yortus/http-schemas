d4236a7ba6967387f31c6741089e48abde851153
Troy Gerwien <yortus@gmail.com>
2022-02-20 11:50:00 +0800

    fix affectd tests

--- src/test/client-server-v0-api.test.ts
+++ src/test/client-server-v0-api.test.ts
@@ -45,6 +45,6 @@ describe('Implementing a HTTP client and server (v0.x API)', () => {
     it('Server-side validation error', async () => {
         const invalid = await client.post('/sum', {body: [1, '2', 3, 4] as number[]});
         expect(invalid).to.include({success: false, code: 'MY_CUSTOM_VALIDATION_ERROR'});
-        expect(server.logMessages.at(-1)).equals(`Error: [ 1, '2', 3, 4 ] does not conform to type Array<number>`);
+        expect(server.logMessages.at(-1)).equals(`Error: [1, "2", 3, 4] does not conform to type Array<number>`);
     });
 });
